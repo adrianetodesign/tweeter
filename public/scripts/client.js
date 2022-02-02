55cba4434167dd5455e4228a066c5ec7b1eec2f6
adrianetodesign <adrianetodesign@gmail.com>
2022-02-02 23:52:05 +0000

    Implement renderTweets function and remove unnecessary comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,51 +5,45 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
-
-/* <article class="tweet">
-<header>
-  <div class="user">
-    <img src="https://i.imgur.com/73hZDYK.png">
-    <h5>Carl Sagan</h5>
-  </div>
-  <h6>@Cosmos</h6>
-</header>
-<p>Look again at that dot. That's here. That's home. That's us.</p>
-<footer>
-  <h6>7 Days Ago</h6>
-  <div>
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </div>
-</footer>
-</article> */
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(() => {
-  const createTweetElement = function(data) {
+  const createTweetElement = function(tweet) {
     return `
     <article class="tweet">
     <header>
       <div class="user">
-        <img src="${data.user.avatars}">
-        <h5>${data.user.name}</h5>
+        <img src="${tweet.user.avatars}">
+        <h5>${tweet.user.name}</h5>
       </div>
-      <h6>${data.user.handle}</h6>
+      <h6>${tweet.user.handle}</h6>
     </header>
-    <p>${data.content.text}</p>
+    <p>${tweet.content.text}</p>
     <footer>
-      <h6>${timeago.format(data.created_at)}</h6>
+      <h6>${timeago.format(tweet.created_at)}</h6>
       <div>
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -59,9 +53,12 @@ $(() => {
     </article>
     `;
   };
-  
-  const $tweet = createTweetElement(tweetData);
 
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
+  }
+
+  renderTweets(data);
 });
