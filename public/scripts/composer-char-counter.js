a6b46a7a437d9064c2a7414b918203e32049e6d2
adrianetodesign <adrianetodesign@gmail.com>
2022-02-02 01:46:27 +0000

    Add counter functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,19 @@
 $(() => {
-  console.log("Ready to edit the document!");
+  console.log("Document ready!");
+
+  $("#tweet-text").on("input", function() {
+    let charTyped = this.value.length;
+    let charAvail = 140 - charTyped;
+
+    // Grab counter element in the next div.
+    let counter = $(this).parent().next().children(".counter");
+
+    counter.text(charAvail);
+
+    if (charAvail < 0) {
+      counter.addClass("red-text");
+    } else {
+      counter.removeClass("red-text");
+    }
+  })
 });
