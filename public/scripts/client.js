767c08067829849a0e8195c61be1d7948a0e0418
adrianetodesign <adrianetodesign@gmail.com>
2022-02-03 00:34:45 +0000

    Remove unintentional require line and dummy data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function(tweet) {
   let $htmlTweet = `
   <article class="tweet">
@@ -79,11 +53,12 @@ $(() => {
     $.ajax("/tweets", {
       method: "GET"
     }).then((tweets) => {
-      return tweets;
+      renderTweets(tweets);
     }).catch((err) => {
       console.log("An error has occured:", err);
     });
   };
 
-  renderTweets(loadTweets());
+  loadTweets();
+  
 });
