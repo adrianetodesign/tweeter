3594e950b05d2124d2c075a4d86b3e9e1e8ad73d
adrianetodesign <adrianetodesign@gmail.com>
2022-02-04 01:10:15 +0000

    Refactor composer-char-counter to an IIFE

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,21 @@
 (function($) {
 
-$(() => {
-  $(".tweet-text").on("input", function() {
-    let charTyped = this.value.length;
-    let charAvail = 140 - charTyped;
+  $(() => {
+    $(".tweet-text").on("input", function() {
+      let charTyped = this.value.length;
+      let charAvail = 140 - charTyped;
 
-    // Grab counter element in the next div.
-    let counter = $(this).parent().next().children(".counter");
+      // Grab counter element in the next div.
+      let counter = $(this).parent().next().children(".counter");
 
-    counter.text(charAvail);
+      counter.text(charAvail);
 
-    if (charAvail < 0) {
-      counter.addClass("red-text");
-    } else {
-      counter.removeClass("red-text");
-    }
-  })
-});
+      if (charAvail < 0) {
+        counter.addClass("red-text");
+      } else {
+        counter.removeClass("red-text");
+      }
+    })
+  });
 
 })(jQuery);
