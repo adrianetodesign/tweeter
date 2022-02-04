2a61cdf7af495467f1fd8e124510e297788ffdd4
adrianetodesign <adrianetodesign@gmail.com>
2022-02-04 02:10:00 +0000

    Add comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -86,6 +86,9 @@ $(() => {
       });
   });
 
+  //--------- Scroll Functions ----------
+
+  //--- Scrolls back to #new-tweet form
   $("#scroll-new-tweet").click(function() {
     $("html, body").animate({
       scrollTop: $("#new-tweet").offset().top
@@ -94,6 +97,7 @@ $(() => {
     });
   });
 
+  //--- Navigation bg added on scroll
   $(window).scroll(function() {
     if($(this).scrollTop() >= 280) {
       $("nav").addClass("nav-scroll-bg");
@@ -102,7 +106,7 @@ $(() => {
     }
   })
 
-  //--- Function for the return to top of page button.
+  //--- Return to top of page button
   $(window).scroll(function() {
     // Fade button in if user scrolls past 400 pixels. Else, fade out.
     if ($(this).scrollTop() >= 400) {
